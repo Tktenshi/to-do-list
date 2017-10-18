7bb880847ca6b2d8d164d89ba560f18f75517a8f
Toma <tktenshi@mail.ru>
2017-10-18 13:42:38 +0300

    Добавление и валидация введённой задачи

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var filterList = ["All", "Done", "Not done", "Tomorrow", "Week"];
 var taskList = [
-    {done: false, text: "Сделать домашку машку", date: "27.05.17"},
+    {done: false, text: "Сделать домашку ", date: "27.05.17"},
     {done: true, text: "Сделать домашку", date: "07.08.17"},
-    {done: false, text: "Сделать дом", date: ""},
+    {done: false, text: "Сделать домашку машку Сделать домашк дом", date: ""},
     {done: false, text: "Сделать домашкуу", date: ""}
 ];
 var showDate = true;
@@ -24,7 +24,7 @@ function createEl(tagName, parent, className, innerHTML, attributes) {
 
 var article = createEl("article", main, "list");
 createEl("h1", article, "list_h1", "My Lovely To Do List");
-createEl("div", article, "list_img");
+var img = createEl("div", article, "list_img");
 var inpCreateTask = createEl("input", article, "list_input list_el", null, {
     type: "text",
     placeholder: "Create a task",
@@ -33,12 +33,33 @@ var inpCreateTask = createEl("input", article, "list_input list_el", null, {
 });
 
 var line2 = createEl("div", article);
-var inpCreateDate = createEl("input", line2, "list_input list_el", null, {
+var inpCreateDate = createEl("input", line2, "list_input list_el", "18.10.2017", {
     type: "date",
     placeholder: "Due date",
     tabindex: "2"
 });
-var btnAddTask = createEl("button", line2, "list_button list_el violet", "Add Task");
+var flag;
+if (inpCreateDate.valueAsDate === undefined) {
+    flag = true;
+    inpCreateDate.value = new Date().toLocaleDateString();
+}
+inpCreateDate.valueAsDate = new Date();
+
+createEl("button", line2, "list_button list_el violet", "Add Task").onclick = function () {
+    inpCreateTask.focus();
+    if (inpCreateTask.value === "") {
+        img.classList.add('error--img');
+        inpCreateTask.classList.add('error--input');
+        return;
+    }
+    img.classList.remove('error--img');
+    inpCreateTask.classList.remove('error--input');
+    var duedate = flag ? inpCreateDate.value : inpCreateDate.valueAsDate ? inpCreateDate.valueAsDate.toLocaleDateString() : "";
+    taskList.push({done: false, text: inpCreateTask.value, date: duedate});
+    console.log(taskList);
+    addRow(taskList.length - 1);
+    inpCreateTask.value = "";
+};
 
 var noWrap1 = createEl("span", article, "no-wrap");
 createEl("label", noWrap1, null, "Filter: ", {for: "list-filter"});
@@ -47,6 +68,8 @@ var selFilter = createEl("select", noWrap1, "list_input list_el", null, {id: "li
 for (var i = 0; i < filterList.length; i++) {
     var option = createEl("option", selFilter, null, filterList[i]);
 }
+
+/////////////////////////////ВСТАВИТЬ ТУТ ПРОБЕЛ
 
 var noWrap2 = createEl("span", article, "no-wrap");
 var chbShowDate = createEl("input", noWrap2, "list_check", null, {
@@ -60,20 +83,25 @@ createEl("label", noWrap2, null, " Show date field", {for: "list-check"});
 var tableCont = createEl("div", article, "list_table-container");
 var table = createEl("table", tableCont, "list_table");
 
-for (var k = 0; k < taskList.length; k++) {
-    var row = createEl("tr", table);
+for (var m = 0; m < taskList.length; m++) {
+    addRow(m);
+}
+
+function addRow(k) {
+    var row = createEl("tr", table, taskList[k].done && "line-through");
     row.onclick = function () {
         window.el = this;
         console.log(this.rowIndex);
     };
     var td1 = createEl("td", row, "list_table-checkbox");
-    var check = taskList[k].done ? "checked" : null;
-    createEl("input", td1, "list_check", null, {type: "checkbox", id: k}).checked = check;
+    createEl("input", td1, "list_check", null, {type: "checkbox", id: k}).checked = taskList[k].done;
     createEl("label", td1, "violet list_el list_label-check", null, {for: k});
 
-    createEl("td", row, null, taskList[k].text);
+    createEl("td", row, "wrap-word", taskList[k].text);
     createEl("td", row, "list_table-date", taskList[k].date);
-
-    var td4 = createEl("td", row);
-    createEl("button", td4, "list_button--del list_el violet");
+    createEl("button", createEl("td", row), "list_button--del list_el violet");
 }
+
+
+// el.classList.toggle('line-through');
+// el.classList.add('myCssClass');
